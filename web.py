ed39fd2fc456da4378caaf78ad7d0841ee6b40c1
Biscuitchoko <lucastardy636@gmail.com>
2025-05-01 18:14:43 +0200

    Update web.py

--- web.py
+++ web.py
@@ -1,11 +1,15 @@
-import os
-from flask import Flask # type: ignore
+from flask import Flask
+import threading
 
-app = Flask(__name__)
+app = Flask('')
 
 @app.route('/')
 def home():
-    return "HexaLock bot is running!"
+    return "Bot is alive!"
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    thread = threading.Thread(target=run)
+    thread.start()
